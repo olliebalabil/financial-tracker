0d0fe80ffce8b9e670568dcd50da49b975f2f7d4
olliebalabil <oliverbalabil@gmail.com>
2024-03-06 16:11:24 +0000

    css

--- frontend/src/components/TransactionCard/index.jsx
+++ frontend/src/components/TransactionCard/index.jsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useTransaction, useBalance } from '../../contexts'
+import binIcon from "../../assets/bin.png"
 
-export default function TransactionCard({transaction}) {
-  const {transactions,setTransactions} = useTransaction()
-  const {balance, setBalance} = useBalance()
+export default function TransactionCard({ transaction }) {
+  const { transactions, setTransactions } = useTransaction()
+  const { balance, setBalance } = useBalance()
 
   const handleDelete = () => {
     const deleteTransaction = async () => {
@@ -14,30 +15,33 @@ export default function TransactionCard({transaction}) {
             transaction_id: transaction.transaction_id,
             user_id: transaction.user_id
           }),
-          headers:{
-            "Content-Type":"application/json"
+          headers: {
+            "Content-Type": "application/json"
           }
         }
-        const response = await fetch("https://financial-tracker-auth.onrender.com/transaction/transaction",options)
+        const response = await fetch("https://financial-tracker-auth.onrender.com/transaction/transaction", options)
         const data = await response.json()
-        if (response.status==200) {
-          setTransactions([...transactions].filter(t=>t.transaction_id!=data.transaction_id))
-         
-          setBalance([balance[0],(parseFloat(balance[1])+parseFloat(data.amount)).toFixed(2)])
-        } 
+        if (response.status == 200) {
+          setTransactions([...transactions].filter(t => t.transaction_id != data.transaction_id))
+
+          setBalance([balance[0], (parseFloat(balance[1]) + parseFloat(data.amount)).toFixed(2)])
+        }
       } catch (err) {
-        console.error({error:err})
+        console.error({ error: err })
       }
     }
     deleteTransaction()
   }
 
   return (
-    <div key={transaction.transaction_id}>
-          <h1>{transaction.reference}</h1>
-          <h2>{balance[0]}{parseFloat(transaction.amount).toFixed(2)}</h2>
-          <h2>{transaction.category}</h2>
-          <button onClick={handleDelete}>Delete</button>
-        </div>
+    <div className={`transaction-card`} key={transaction.transaction_id}>
+      <div className={`circle ${(transaction.amount > 0) ? "withdraw" : "deposit"}`}></div>
+      <h1 className="title">{transaction.reference}</h1>
+      <div className='amount-category'>
+        <h2 className='amount'>{balance[0]}{Math.abs(parseFloat(transaction.amount)).toFixed(2)}</h2>
+        <h2>{transaction.category}</h2>
+      </div>
+      <img src={binIcon} onClick={handleDelete} alt="delete" />
+    </div>
   )
 }
--- frontend/src/components/TransactionForm/index.jsx
+++ frontend/src/components/TransactionForm/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { useTransaction,useBalance } from '../../contexts'
+import { useTransaction, useBalance } from '../../contexts'
 export default function TransactionForm() {
-  const {transactions,setTransactions} = useTransaction()
-  const {balance,setBalance} = useBalance()
-  const [reference,setReference] = useState()
-  const [amount,setAmount] = useState(0)
-  const [category,setCategory] = useState("Other")
+  const { transactions, setTransactions } = useTransaction()
+  const { balance, setBalance } = useBalance()
+  const [reference, setReference] = useState()
+  const [amount, setAmount] = useState(0)
+  const [category, setCategory] = useState("Other")
+  const [deposit,setDeposit] = useState(false)
 
   const handleReference = (e) => {
     setReference(e.target.value)
@@ -16,30 +17,37 @@ export default function TransactionForm() {
   const handleCategory = (e) => {
     setCategory(e.target.value)
   }
+  const handleToggle = (e) =>{
+    setDeposit(!deposit)
+  }
   const handleSubmit = (e) => {
     e.preventDefault()
     const createTransaction = async () => {
       try {
+        let newAmount = amount
+        if (deposit) {
+          newAmount = -newAmount
+        }
         const options = {
           method: "POST",
           body: JSON.stringify({
             user_id: sessionStorage.getItem("user_id"),
             reference: reference,
-            amount: amount,
-            category: category          
+            amount: newAmount,
+            category: category
           }),
           headers: {
             "Content-Type": "application/json"
-          } 
+          }
         }
         const response = await fetch("https://financial-tracker-auth.onrender.com/transaction/transaction", options)
         if (response.status == 200) {
           const data = await response.json()
-          setTransactions([data,...transactions])
-          setBalance([balance[0],(balance[1]-data.amount).toFixed(2)])
+          setTransactions([data, ...transactions])
+          setBalance([balance[0], (balance[1] - data.amount).toFixed(2)])
         }
       } catch (err) {
-        console.error({error:err})
+        console.error({ error: err })
       }
     }
     createTransaction()
@@ -51,15 +59,29 @@ export default function TransactionForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder='Reference' onChange={handleReference} value={reference}/>
+      <input type="text" placeholder='Reference' onChange={handleReference} value={reference} />
       <label>Amount: </label>
-      <input type="number" min="0" step="0.01" onChange={handleAmount} value={amount}/>
+      <input type="number" min="0" step="0.01" onChange={handleAmount} value={amount} />
+      <label>Deposit:</label>
+      <label class="switch">
+        
+        <input type="checkbox" onChange={handleToggle}/>
+          <span class="slider round"></span>
+      </label>
+
       <label>Category: </label>
       <select onChange={handleCategory} value={category}>
-        
-        <option value="Food">Food</option>
-        <option value="Bills">Bills</option>
-        <option value="Other" >Other</option>
+
+        <option value="Miscellaneous">Miscellaneous</option>
+        <option value="Groceries">Groceries</option>
+        <option value="Housing">Housing</option>
+        <option value="Utilities">Utilities</option>
+        <option value="Travel">Travel</option>
+        <option value="Entertainment">Entertainment</option>
+        <option value="Income">Income</option>
+
+
+
       </select>
       <input type="submit" />
     </form>
