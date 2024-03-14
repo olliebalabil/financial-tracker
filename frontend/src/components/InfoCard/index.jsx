f6c5e950dd7bed19ed33d687ef66472e0ba79d30
olliebalabil <oliverbalabil@gmail.com>
2024-03-14 16:20:17 +0000

    test4

--- frontend/src/components/InfoCard/index.jsx
+++ frontend/src/components/InfoCard/index.jsx
@@ -25,7 +25,7 @@ export default function InfoCard() {
 
     <div className='info-card'>
       {(balance[1]>=0)? <h1>Balance: {balance[0]}{balance[1]}</h1> :
-      <h1>Balance: -{balance[0]}{balance[1]}</h1>}
+      <h1>Balance: -{balance[0]}{Math.abs(balance[1])}</h1>}
      
     </div>
   )
